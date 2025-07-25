ec9b4092d856409f799a4fa46ea3c0924f915da5
Taras Bairakivskyi <ansel87@gmail.com>
2025-07-26 01:57:08 +0300

    Add files via upload

---
+++ lesson_16/js/script.js
@@ -0,0 +1,16 @@
+document.addEventListener('click', documentActions)
+
+function documentActions(e) {
+	const targetElement = e.target
+	if (targetElement.closest('.menu__arrow')) {
+		targetElement.closest('.menu__item').classList.toggle('active')
+	}
+	if (targetElement.closest('.icon-menu')) {
+		document.body.classList.toggle('menu-open')
+	}
+}
+
+
+
+
+
